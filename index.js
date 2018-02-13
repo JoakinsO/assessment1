d60f0d8147155230617de8cb0994487065e2f85c
Joakins Onyango <joakinsonyango05@gmail.com>
2018-02-12 22:02:41 -0800

    added 6 files

---
+++ index.js
@@ -0,0 +1,49 @@
+//var img = document.createElement("img");
+//img.src = "imgs/i1.jpg";
+//var app = document.getElementById.("ch1");
+//app.appendChild(img)
+
+document.getElementById("BG1").addEventListener("click", function(){
+    document.getElementById("ch1").innerHTML="<img src ='imgs/i1.jpg'/>";
+   
+});
+document.getElementById("BG1").addEventListener("click", function(){
+    document.getElementById("ch2").innerHTML="<img src ='imgs/i2.jpg'/>";
+});
+document.getElementById("BG1").addEventListener("click", function(){
+    document.getElementById("ch3").innerHTML="<img src ='imgs/i3.jpg'/>";
+});
+
+
+document.getElementById("BG2").addEventListener("click", function(){
+    document.getElementById("ch1").innerHTML="<img src ='imgs/i4.jpg'/>";
+});
+document.getElementById("BG2").addEventListener("click", function(){
+    document.getElementById("ch2").innerHTML="<img src ='imgs/i5.jpg'/>";
+});
+document.getElementById("BG2").addEventListener("click", function(){
+    document.getElementById("ch3").innerHTML="<img src ='imgs/i6.jpg'/>";
+});
+
+
+document.getElementById("BG3").addEventListener("click", function(){
+    document.getElementById("ch1").innerHTML="<img src ='imgs/i7.jpg'/>";
+});
+document.getElementById("BG3").addEventListener("click", function(){
+    document.getElementById("ch2").innerHTML="<img src ='imgs/i8.jpg'/>";
+});
+document.getElementById("BG3").addEventListener("click", function(){
+    document.getElementById("ch3").innerHTML="<img src ='imgs/i9.jpg'/>";
+});
+
+
+document.getElementById("BG4").addEventListener("click", function(){
+    document.getElementById("ch1").innerHTML="<img src ='imgs/i10.jpg'/>";
+});
+document.getElementById("BG2").addEventListener("click", function(){
+    document.getElementById("ch2").innerHTML="<img src ='imgs/i11.jpg'/>";
+});
+document.getElementById("BG4").addEventListener("click", function(){
+    document.getElementById("ch3").innerHTML="<img src ='imgs/i12.jpg'/>";
+});
+
